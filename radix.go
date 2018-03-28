6b08691c8be29e6e4be0a44dd029d198add19abe
freepk <freepk@mail.ru>
2018-03-28 15:02:33 +0300

    Update radix.go

--- radix.go
+++ radix.go
@@ -15,14 +15,23 @@ func init() {
 func Ints(source, buffer []int, size int) {
 	var shift byte
 	var counters []int
-	var pos, i int
+	var pos, i, curr, prev int
 	var num byte
+	var sorted bool
 	counters = make([]int, numCounters)
-	for shift = 0; shift < (intSize * 8); shift = shift + 8 {
+	for shift = 0; shift < (intSize * 8); shift += 8 {
 		copy(counters, emptyCounters)
+		sorted = true
+		prev = source[0]
 		for i = 0; i < size; i++ {
-			num = byte(source[i] >> shift)
+			curr = source[i]
+			sorted = sorted && (prev <= curr)
+			num = byte(curr >> shift)
 			counters[num]++
+			prev = curr
+		}
+		if sorted {
+			return
 		}
 		pos = 0
 		for i = 0; i < numCounters; i++ {
@@ -37,4 +46,3 @@ func Ints(source, buffer []int, size int) {
 		source, buffer = buffer, source
 	}
 }
-
